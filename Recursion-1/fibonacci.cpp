d97aff1b633e9e8d180943d470638d61d98d9059
Gifar Al Anshar <86858662+giifrr@users.noreply.github.com>
2022-07-05 20:14:48 +0700

    Create fibonacci.cpp

---
+++ Recursion-1/fibonacci.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibonacci(int n)
+{
+  if (n == 0)
+    return 0;
+  if (n == 1)
+    return 1;
+  return fibonacci(n - 2) + fibonacci(n - 1);
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  cout << fibonacci(n);
+}
